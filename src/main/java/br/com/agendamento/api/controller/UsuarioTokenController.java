85e2a2192527b4d24b910bd4f4324b29db8890fc
sonde756 <edsonrafael756@gmail.com>
2023-09-16 22:10:40 -0300

    Revert "mapeamento UsuarioTokenController"
    This reverts commit 1eb04f29d253aeaa850660010c08cbca6cf1453e.

--- src/main/java/br/com/agendamento/api/controller/UsuarioTokenController.java
+++ src/main/java/br/com/agendamento/api/controller/UsuarioTokenController.java
@@ -3,26 +3,28 @@ package br.com.agendamento.api.controller;
 
 import br.com.agendamento.api.model.UsuarioToken;
 import br.com.agendamento.api.service.token.UsuarioTokenService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Controller email
  *
  * @author Edson Rafael
  */
+@Api(value = "usuarioToken", tags = { "usuarioToken" })
 @RestController
+@RequestMapping("/confirmacao-email")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UsuarioTokenController {
 
     @Autowired
     private UsuarioTokenService service;
 
-    @GetMapping("/{email}/{codigoConfirmacao}")
-    public ResponseEntity<UsuarioToken> update(@PathVariable String email, @PathVariable String codigoConfirmacao) {
-        service.confirmarEmailComToken(email, codigoConfirmacao);
+    @GetMapping("/{email}/{token}")
+    public ResponseEntity<UsuarioToken> update(@PathVariable String email, @PathVariable String token) {
+        service.confirmarEmailComToken(email, token);
         return ResponseEntity.noContent().build();
     }
 
